170b0f090c8e4049da73183194dce91cf7b073bf
Nicolas <nicoga97@gmail.com>
2019-04-18 19:24:42 -0500

    First commit

---
+++ app/src/main/java/com/nicoga/taskplanner/LaunchActivity.java
@@ -0,0 +1,30 @@
+package com.nicoga.taskplanner;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    public static final String TOKEN_KEY = "TOKEN_KEY";
+
+    @Override
+    protected void onCreate( @Nullable Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        SharedPreferences sharedPref =
+                getSharedPreferences( getString( R.string.login_preferences ), Context.MODE_PRIVATE );
+
+        if(sharedPref.contains(TOKEN_KEY)){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }else{
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+}
